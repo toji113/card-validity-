59e1291e3c168e9b23ffe2aedc41e726d6a0d517
toji113 <abhishekbhilala66@gmail.com>
2024-05-14 23:15:56 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,16 @@
+# credit card validity 
+def validate(card_number):
+  def digits_of(n):
+    return [int(d) for d in str(n)]
+  digits = digits_of(card_number)
+  odd_digits = digits[-1::-2]
+  even_digits = digits[-2::-2]
+  checksum = 0 
+  checksum += sum(odd_digits)
+  for d in even_digits:
+    checksum =+ sum(digits_of(d*2))
+  return checksum % 10
+if validate(input(" ENTER NUMBER \n")) ==0:
+  print("card is valid")
+else:
+  print("card is unvalid")
